54d519c7ae5fb7a05f836ba2b78ea24f2c5bcb35
Sam Ou <lll810502@gmail.com>
2018-12-11 10:37:04 +0800

    check and write

--- pruducts.py
+++ pruducts.py
@@ -1,4 +1,14 @@
 pruducts = []
+with open('pruducts.csv', 'r') as f:
+    for line in f:
+        if '商品,價格' in line:
+            continue
+        name, price = line.strip().split(',') 
+        pruducts.append([name, price])
+
+print(pruducts)
+
+
 while True:
     name = input('請輸入商品名稱： ')
     if name == 'q':
@@ -9,3 +19,8 @@ print(pruducts)
 
 for p in pruducts:
     print(p[0], '的價格是', p[1])
+
+with open('pruducts.csv', 'w') as f:
+    f.write('商品,價格\n')
+    for p in pruducts:
+        f.write(p[0] + ',' + p[1] + '\n')
