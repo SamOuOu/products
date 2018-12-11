c88e10e6ed1e681dfc3fa6d781c1fdceb51be578
Sam Ou <lll810502@gmail.com>
2018-12-11 11:11:24 +0800

    add check file

--- pruducts.py
+++ pruducts.py
@@ -1,14 +1,23 @@
+#檢查檔案是否存在
+import os
+
 pruducts = []
-with open('pruducts.csv', 'r') as f:
-    for line in f:
-        if '商品,價格' in line:
-            continue
-        name, price = line.strip().split(',') 
-        pruducts.append([name, price])
 
-print(pruducts)
+if os.path.isfile('pruducts.csv'):
+    print('UUU')
+    #讀取
+    with open('pruducts.csv', 'r') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue
+            name, price = line.strip().split(',') 
+            pruducts.append([name, price])
+    print(pruducts)
 
+else:
+    print('NNN')
 
+#輸入新增
 while True:
     name = input('請輸入商品名稱： ')
     if name == 'q':
@@ -17,9 +26,11 @@ while True:
     pruducts.append([name, price])
 print(pruducts)
 
+#印出每項
 for p in pruducts:
     print(p[0], '的價格是', p[1])
 
+#寫入檔案
 with open('pruducts.csv', 'w') as f:
     f.write('商品,價格\n')
     for p in pruducts:
